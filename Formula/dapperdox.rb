ee5908f96af51c2b1d5cdb74b421ac6e9742da8f
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2017-04-12 22:44:26 +0100

    Patch config to override default assets path and install in /usr/local/share

--- Formula/dapperdox.rb
+++ Formula/dapperdox.rb
@@ -6,6 +6,9 @@ class Dapperdox < Formula
 
   depends_on "go" => :build
 
+  # patch the default assets path to permit assets to be installed in /usr/local/share
+  patch :DATA
+
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/DapperDox/dapperdox").install buildpath.children
@@ -14,6 +17,7 @@ class Dapperdox < Formula
       system "go", "get"
       system "go", "build", "-o", bin/"dapperdox"
       prefix.install_metafiles
+      (pkgshare/"assets").install Dir["assets/*"]
       (pkgshare/"examples").install Dir["examples/*"]
     end
   end
@@ -22,3 +26,18 @@ class Dapperdox < Formula
     assert (bin/"dapperdox").exist?
   end
 end
+
+__END__
+diff --git a/config/config.go b/config/config.go
+index affc6a7..0b96721 100644
+--- a/config/config.go
++++ b/config/config.go
+@@ -39,7 +39,7 @@ func Get() (*config, error) {
+ 	cfg = &config{
+ 		BindAddr:         "localhost:3123",
+ 		SpecDir:          "spec",
+-		DefaultAssetsDir: "assets",
++		DefaultAssetsDir: "/usr/local/share/dapperdox/assets",
+ 		LogLevel:         "info",
+ 		SiteURL:          "http://localhost:3123/",
+ 		ShowAssets:       false,
